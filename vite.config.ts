8fa927bed8f991343ce41387ecc438761825d365
Darko Ilic <darko.ilic83@gmail.com>
2026-04-16 18:07:30 +0200

    Izmenjeni pakckage vraćena stara linija koda za build, a u viteconfig dodate linije koda za koje će povlačiti html stranice iz roota

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,18 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    plugins: [
-        tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
+    build: {
+        rollupOptions: {
+            input: {
+                index: new URL('./index.html', import.meta.url).pathname,
+                '404': new URL('./404.html', import.meta.url).pathname,
+                kontakt: new URL('./kontakt.html', import.meta.url).pathname,
+                'o-nama': new URL('./o-nama.html', import.meta.url).pathname,
+                portfolio: new URL('./portfolio.html', import.meta.url).pathname,
+                'seo-optimizacija': new URL('./seo-optimizacija.html', import.meta.url).pathname,
+                'izrada-web-sajta': new URL('./izrada-web-sajta.html', import.meta.url).pathname,
+            },
+        },
+    },
 })
